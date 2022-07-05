e5d01934a06729cafc4efa3b93009c0659bd19c5
Andriy125 <57405296+Andriy125@users.noreply.github.com>
2022-07-05 17:34:57 +0300

    second commit

--- init.sh
+++ init.sh
@@ -5,12 +5,10 @@ sudo apt-get update -y
 sudo apt install yarn -y
 sudo apt install mariadb -y
 sudo mkdir app && cd app
-#sudo git clone https://github.com/Sofi-Fed-23/cloud_technologies_demo.git && cd front
-cd front
+sudo git clone https://github.com/Andriy125/Diplom.git && cd Diplom/front
 sudo yarn install -y
 sudo yarn start
-sudo cd ..
-sudo cd back
+sudo cd ../back
 sudo cp .env.example .env
 sudo yarn install -y
 sudo yarn start
